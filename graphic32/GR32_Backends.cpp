3a80c0972678daad93b72c86f39d0c9bc4adc1e9
hyperiris <hyperiris@fca59bba-de1c-11dd-a738-5fe2e3cfbcc5>
2014-03-20 13:28:47 +0000

    wip

--- graphic32/GR32_Backends.cpp
+++ graphic32/GR32_Backends.cpp
@@ -8,36 +8,36 @@ void RequireBackendSupport(TargetBitmap: TCustomBitmap32;
   RequiredInterfaces: array of TGUID;
   Mode: TRequireOperatorMode; UseOptimizedDestructiveSwitchMethod: Boolean;
   out ReleasedBackend: TCustomBackend);
-var
-  I: Integer;
-  Supported: Boolean;
 {
-  Supported := False;
-  for I := Low(RequiredInterfaces) to High(RequiredInterfaces) do
-  {
-    Supported := Supports(TargetBitmap.Backend, RequiredInterfaces[I]);
-    if ((Mode = romAnd) and not Supported) or
-      ((Mode = romOr) and Supported) then
-      Break;
-  }
+	int I;
+	BOOL Supported = False;
 
-  if not Supported then
-  {
-    if UseOptimizedDestructiveSwitchMethod then
-      TargetBitmap.SetSize(0, 0); // Reset size so we avoid the buffer copy during back-end switch
+	for I : = Low(RequiredInterfaces) to High(RequiredInterfaces) do
+	{
+	Supported: = Supports(TargetBitmap.Backend, RequiredInterfaces[I]);
+		if ((Mode = romAnd) and not Supported) or
+			((Mode = romOr) and Supported) then
+			Break;
+	}
 
-    ReleasedBackend := TargetBitmap.ReleaseBack}
+	if not Supported then
+	{
+		if UseOptimizedDestructiveSwitchMethod then
+		TargetBitmap.SetSize(0, 0); // Reset size so we avoid the buffer copy during back-end switch
 
-    // TODO: Try to find a back-end that supports the required interfaces
-    //       instead of resorting to the default platform back-end class...
-    TargetBitmap.Backend := GetPlatformBackendClass.Create;
-  end
-  else
-    ReleasedBackend := nil;
+	ReleasedBackend: = TargetBitmap.ReleaseBack }
+
+		// TODO: Try to find a back-end that supports the required interfaces
+		//       instead of resorting to the default platform back-end class...
+		TargetBitmap.Backend : = GetPlatformBackendClass.Create;
+		end
+	else
+	
+	ReleasedBackend : = nil;
 }
 
 void RestoreBackend(TargetBitmap: TCustomBitmap32; const SavedBackend: TCustomBackend);
 {
-  if Assigned(SavedBackend) then
-    TargetBitmap.Backend := SavedBack}
+	if Assigned(SavedBackend)
+		TargetBitmap.Backend : = SavedBack
 }
--- graphic32/GR32_Backends.h
+++ graphic32/GR32_Backends.h
@@ -5,86 +5,89 @@
 #include "GR32_Containers.h"
 #include "GR32_Image.h"
 
+class ITextSupport
+{
+	virtual void Textout(X, Y: Integer; const Text : String) = 0;
+	virtual void Textout(X, Y: Integer; const ClipRect : TRect; const Text : String) = 0;
+	virtual void Textout(var DstRect : TRect; const Flags : Cardinal; const Text : String) = 0;
+	virtual TSize TextExtent(const Text : String) = 0;
 
-type
-  ITextSupport = interface(IUnknown)
-  ['{225997CC-958A-423E-8B60-9EDE0D3B53B5}']
-    procedure Textout(X, Y: Integer; const Text: String); overload;
-    procedure Textout(X, Y: Integer; const ClipRect: TRect; const Text: String); overload;
-    procedure Textout(var DstRect: TRect; const Flags: Cardinal; const Text: String); overload;
-    function  TextExtent(const Text: String): TSize;
+	virtual void TextoutW(X, Y: Integer; const Text : Widestring) = 0;
+	virtual void TextoutW(X, Y: Integer; const ClipRect : TRect; const Text : Widestring) = 0;
+	virtual void TextoutW(var DstRect : TRect; const Flags : Cardinal; const Text : Widestring) = 0;
+	virtual TSize TextExtentW(const Text : Widestring) = 0;
+};
 
-    procedure TextoutW(X, Y: Integer; const Text: Widestring); overload;
-    procedure TextoutW(X, Y: Integer; const ClipRect: TRect; const Text: Widestring); overload;
-    procedure TextoutW(var DstRect: TRect; const Flags: Cardinal; const Text: Widestring); overload;
-    function  TextExtentW(const Text: Widestring): TSize;
-  end;
+class IFontSupport
+{
+	virtual function GetOnFontChange : TNotifyEvent = 0;
+	virtual void SetOnFontChange(Handler: TNotifyEvent) = 0;
+	virtual function GetFont : TFont = 0;
+	virtual void SetFont(const Font : TFont) = 0;
 
-  IFontSupport = interface(IUnknown)
-  ['{67C73044-1EFF-4FDE-AEA2-56BFADA50A48}']
-    function GetOnFontChange: TNotifyEvent;
-    procedure SetOnFontChange(Handler: TNotifyEvent);
-    function GetFont: TFont;
-    procedure SetFont(const Font: TFont);
+	virtual void UpdateFont = 0;
+	virtual property Font: TFont read GetFont write SetFont;
+	virtual property OnFontChange: TNotifyEvent read GetOnFontChange write SetOnFontChange;
+};
 
-    procedure UpdateFont;
-    property Font: TFont read GetFont write SetFont;
-    property OnFontChange: TNotifyEvent read GetOnFontChange write SetOnFontChange;
-  end;
+class ICanvasSupport
+{
+	virtual function GetCanvasChange : TNotifyEvent = 0;
+	virtual void SetCanvasChange(Handler: TNotifyEvent) = 0;
+	virtual function GetCanvas : TCanvas = 0;
 
-  ICanvasSupport = interface(IUnknown)
-  ['{5ACFEEC7-0123-4AD8-8AE6-145718438E01}']
-    function GetCanvasChange: TNotifyEvent;
-    procedure SetCanvasChange(Handler: TNotifyEvent);
-    function GetCanvas: TCanvas;
+	virtual void DeleteCanvas = 0;
+	virtual function CanvasAllocated : Boolean = 0;
 
-    procedure DeleteCanvas;
-    function CanvasAllocated: Boolean;
+	virtual property Canvas: TCanvas read GetCanvas;
+	virtual property OnCanvasChange: TNotifyEvent read GetCanvasChange write SetCanvasChange;
+};
 
-    property Canvas: TCanvas read GetCanvas;
-    property OnCanvasChange: TNotifyEvent read GetCanvasChange write SetCanvasChange;
-  end;
+class IDeviceContextSupport
+{
+	virtual function GetHandle : HDC = 0;
 
-  IDeviceContextSupport = interface(IUnknown)
-  ['{DD1109DA-4019-4A5C-A450-3631A73CF288}']
-    function GetHandle: HDC;
+	virtual void Draw(const DstRect, SrcRect: TRect; hSrc: HDC) = 0;
+	virtual void DrawTo(hDst: HDC; DstX, DstY: Integer); overload = 0;
+	virtual void DrawTo(hDst: HDC; const DstRect, SrcRect: TRect); overload = 0;
 
-    procedure Draw(const DstRect, SrcRect: TRect; hSrc: HDC);
-    procedure DrawTo(hDst: HDC; DstX, DstY: Integer); overload;
-    procedure DrawTo(hDst: HDC; const DstRect, SrcRect: TRect); overload;
+	virtual property Handle: HDC read GetHandle;
+};
 
-    property Handle: HDC read GetHandle;
-  end;
+class IBitmapContextSupport
+{
+	virtual function GetBitmapInfo : TBitmapInfo = 0;
+	virtual function GetBitmapHandle : THandle = 0;
 
-  IBitmapContextSupport = interface(IUnknown)
-  ['{DF0F9475-BA13-4C6B-81C3-D138624C4D08}']
-    function GetBitmapInfo: TBitmapInfo;
-    function GetBitmapHandle: THandle;
+	virtual property BitmapInfo: TBitmapInfo read GetBitmapInfo;
+	virtual property BitmapHandle: THandle read GetBitmapHandle;
+};
 
-    property BitmapInfo: TBitmapInfo read GetBitmapInfo;
-    property BitmapHandle: THandle read GetBitmapHandle;
-  end;
+class IPaintSupport
+{
+	virtual void ImageNeeded = 0;
+	virtual void CheckPixmap = 0;
+	virtual void DoPaint(ABuffer: TBitmap32; AInvalidRects: TRectList; ACanvas: TCanvas; APaintBox: TCustomPaintBox32) = 0;
+};
 
-  IPaintSupport = interface(IUnknown)
-  ['{CE64DBEE-C4A9-4E8E-ABCA-1B1FD6F45924}']
-    procedure ImageNeeded;
-    procedure CheckPixmap;
-    procedure DoPaint(ABuffer: TBitmap32; AInvalidRects: TRectList; ACanvas: TCanvas; APaintBox: TCustomPaintBox32);
-  end;
-
-  TRequireOperatorMode = (romAnd, romOr);
+enum TRequireOperatorMode
+{
+	romAnd,
+	romOr
+};
 
 // Helper functions to temporarily switch the back-end depending on the required interfaces
 
-procedure RequireBackendSupport(TargetBitmap: TCustomBitmap32;
+void RequireBackendSupport(TargetBitmap: TCustomBitmap32;
   RequiredInterfaces: array of TGUID;
   Mode: TRequireOperatorMode; UseOptimizedDestructiveSwitchMethod: Boolean;
   out ReleasedBackend: TCustomBackend);
 
-procedure RestoreBackend(TargetBitmap: TCustomBitmap32; const SavedBackend: TCustomBackend);
+void RestoreBackend(TargetBitmap: TCustomBitmap32; const SavedBackend: TCustomBackend);
 
+/*
 resourcestring
   RCStrCannotAllocateDIBHandle = 'Can''t allocate the DIB handle';
   RCStrCannotCreateCompatibleDC = 'Can''t create compatible DC';
   RCStrCannotSelectAnObjectIntoDC = 'Can''t select an object into DC';
-
+*/
